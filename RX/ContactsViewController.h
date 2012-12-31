f171a39f4f7ff3d0aff8518e967184885f968e56
Akhilesh Gupta <akgupta@linkedin.com>
2012-12-31 13:35:04 -0600

    Adding contactsviewcontroller

---
+++ RX/ContactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.h
+//  RX
+//
+//  Created by Akhilesh Gupta on 12/31/12.
+//  Copyright (c) 2012 Akhilesh Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsViewController : UITableViewController
+
+@end
