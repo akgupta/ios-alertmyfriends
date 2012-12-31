489a8128e97c8ce97f9692be7bb13bf7f7a7e357
Akhilesh Gupta <akgupta@linkedin.com>
2012-12-31 13:19:17 -0600

    Initial Commit

---
+++ RX/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RX
+//
+//  Created by Akhilesh Gupta on 12/31/12.
+//  Copyright (c) 2012 Akhilesh Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
