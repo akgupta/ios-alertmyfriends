ef41b5bf23c599b9d99dcbe4ea7ebd5f6857ad9e
Akhilesh Gupta <akgupta@linkedin.com>
2013-01-05 17:17:09 -0800

    SMS on shake with reverse geocoded location

--- RX/ContactsViewController.h
+++ RX/ContactsViewController.h
@@ -9,13 +9,15 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 #import <AddressBookUI/AddressBookUI.h>
+#import <MessageUI/MessageUI.h>
 
-@interface ContactsViewController : UITableViewController <CLLocationManagerDelegate, ABPeoplePickerNavigationControllerDelegate>
+@interface ContactsViewController : UITableViewController <CLLocationManagerDelegate, ABPeoplePickerNavigationControllerDelegate, MFMessageComposeViewControllerDelegate>
 
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (strong, nonatomic) CLLocationManager *locationManager;
 @property (strong, nonatomic) CLLocation *currentLocation;
 @property (strong, nonatomic) NSMutableArray *selectedContacts;
 @property (strong, nonatomic) UIBarButtonItem *addButton;
+@property (strong, nonatomic) CLGeocoder *geoCoder;
 
 @end
--- RX/ContactsViewController.m
+++ RX/ContactsViewController.m
@@ -20,6 +20,7 @@
 @synthesize currentLocation = _currentLocation;
 @synthesize selectedContacts = _selectedContacts;
 @synthesize addButton = _addButton;
+@synthesize geoCoder = _geoCoder;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -39,8 +40,32 @@
 {
     if (motion == UIEventSubtypeMotionShake)
     {
-        NSLog(@"%@", @"Shake");
+        NSLog(@"INFO:%@", @"Shake");
         // reverse geocode and send as text message
+        [_geoCoder reverseGeocodeLocation:_currentLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+            if (error) {
+                NSLog(@"ERROR:There was a reverse geocoding error\n%@", [error description]);
+            }
+            // Iterate through all of the placemarks returned
+            // and output them to the console
+            for(CLPlacemark *placemark in placemarks) {
+                NSLog(@"INFO:Address:\n%@", ABCreateStringWithAddressDictionary(placemark.addressDictionary, YES));
+            }
+            CLPlacemark *placemark = [placemarks lastObject];
+            NSString *address = ABCreateStringWithAddressDictionary(placemark.addressDictionary, YES);
+            // send SMS
+            MFMessageComposeViewController *messageController = [[MFMessageComposeViewController alloc] init];
+            if([MFMessageComposeViewController canSendText]) {
+                [messageController setBody:[NSString stringWithFormat:@"I need help! I'am at \n%@", address]];
+                NSMutableArray *recipients = [NSMutableArray arrayWithCapacity:[_selectedContacts count]];
+                for (Contact *contact in _selectedContacts) {
+                    [recipients addObject:[contact phone]];
+                }
+                [messageController setRecipients:recipients];
+                messageController.messageComposeDelegate = self;
+                [self presentViewController:messageController animated:YES completion:NULL];
+            }
+        }];
     }
 }
 
@@ -62,7 +87,10 @@
     // Location
     _locationManager = [[CLLocationManager alloc] init];
     _locationManager.delegate = self;
+    [_locationManager setDesiredAccuracy:kCLLocationAccuracyNearestTenMeters];
+    [_locationManager setDistanceFilter:100];
     [_locationManager startUpdatingLocation];
+    _geoCoder = [[CLGeocoder alloc] init];
 }
 
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated
@@ -128,12 +156,27 @@
 #pragma mark - CLLocationManagerDelegate
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
 {
-    NSLog(@"Location:lat:%f,lon:%f", newLocation.coordinate.latitude, newLocation.coordinate.longitude);
-    //if we get a location less than 2 minutes old, stop updating location
-    if ( abs([newLocation.timestamp timeIntervalSinceDate: [NSDate date]]) < 120) {
-        [_locationManager stopUpdatingLocation];
-    }
+    // test the age of the location measurement to determine if the measurement is cached
+    // in most cases you will not want to rely on cached measurements
+    NSTimeInterval locationAge = -[newLocation.timestamp timeIntervalSinceNow];
+    if (locationAge > 5.0) return;
+    
+    NSLog(@"INFO:Location:\n lat: %f\n lon: %f\n accuracy: %f\n age: %f", newLocation.coordinate.latitude, newLocation.coordinate.longitude, newLocation.horizontalAccuracy, locationAge);
+    
     _currentLocation = newLocation;
+    
+//    // test that the horizontal accuracy does not indicate an invalid measurement
+//    if (newLocation.horizontalAccuracy < 0) return;
+    
+//    // test the measurement to see if it is more accurate than the previous measurement
+//    if (_currentLocation == nil || _currentLocation.horizontalAccuracy > newLocation.horizontalAccuracy) {
+//        // store the location as the "best effort"
+//        _currentLocation = newLocation;
+//        // test the measurement to see if it meets the desired accuracy
+//        if (newLocation.horizontalAccuracy <= _locationManager.desiredAccuracy) {
+//            [_locationManager stopUpdatingLocation];
+//        }
+//    }
 }
 
 #pragma mark - ABPeoplePickerNavigationControllerDelegate
@@ -191,7 +234,8 @@
     [_selectedContacts addObject:contact];
 }
 
-- (void)fetch {
+- (void)fetch
+{
     NSFetchRequest *request = [[NSFetchRequest alloc] init];
     NSEntityDescription *entity = [NSEntityDescription entityForName:@"Contact" inManagedObjectContext:_managedObjectContext];
     [request setEntity:entity];
@@ -206,6 +250,12 @@
         // Handle the error.
     }
     [self setSelectedContacts:mutableFetchResults];
+}
+
+#pragma mark - MFMessageComposeViewControllerDelegate
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 @end
