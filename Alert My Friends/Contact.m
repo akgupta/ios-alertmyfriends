db9ced759262d52d0d8c7c941713980c5d32944b
Akhilesh Gupta <akgupta@linkedin.com>
2013-01-14 00:14:51 -0800

    Add folder

---
+++ Alert My Friends/Contact.m
@@ -0,0 +1,17 @@
+//
+//  Contact.m
+//  Alert My Friends
+//
+//  Created by Akhilesh Gupta on 1/3/13.
+//  Copyright (c) 2013 Akhilesh Gupta. All rights reserved.
+//
+
+#import "Contact.h"
+
+
+@implementation Contact
+
+@dynamic name;
+@dynamic phone;
+
+@end
